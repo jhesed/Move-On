ba598d88eb2307770719c096f1d8c6ac26c2aa16
Jhesed Tacadena (Ben/TAS/Other <jhesed.tacadena@towerswatson.com>
2017-01-17 17:11:38 +0800

    Added a platform that drops faster than normal

---
+++ Assets/Scripts/PlatformGenerator.cs.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Generate : MonoBehaviour
+{
+    public GameObject[] platforms;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("GeneratePlatform", 1f, 1.5f);
+    }
+
+    void GeneratePlatform()
+    {
+        Instantiate(platforms[0]);
+    }
+}
--- Assets/Scripts/PlatformMover.cs
+++ Assets/Scripts/PlatformMover.cs
@@ -17,44 +17,21 @@ public class PlatformMover : MonoBehaviour {
 
 	Transform _transform;
 	int _myWaypointIndex = 0;		// used as index for My_Waypoints
-	float _moveTime;
-	bool _moving = true;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		_transform = platform.transform;
-		_moveTime = 0f;
-		_moving = true;
 	}
 	
 	// game loop
 	void Update () {
 		// if beyond _moveTime, then start moving
-		if (Time.time >= _moveTime) {
-			Movement();
-		}
+	    Movement();
 	}
 
 	void Movement() {
-		// if there isn't anything in My_Waypoints
-		if ((myWaypoints.Length != 0) && (_moving)) {
-
-			// move towards waypoint
-			_transform.position = Vector3.MoveTowards(_transform.position, myWaypoints[_myWaypointIndex].transform.position, moveSpeed * Time.deltaTime);
-
-			// if the enemy is close enough to waypoint, make it's new target the next waypoint
-			if(Vector3.Distance(myWaypoints[_myWaypointIndex].transform.position, _transform.position) <= 0) {
-				_myWaypointIndex++;
-				_moveTime = Time.time + waitAtWaypointTime;
-			}
+  
+        _transform.position = Vector3.MoveTowards(_transform.position, _transform.position + new Vector3(100, 0, 0), moveSpeed * Time.deltaTime);
 			
-			// reset waypoint back to 0 for looping, otherwise flag not moving for not looping
-			if(_myWaypointIndex >= myWaypoints.Length) {
-				if (loop)
-					_myWaypointIndex = 0;
-				else
-					_moving = false;
-			}
-		}
 	}
 }
